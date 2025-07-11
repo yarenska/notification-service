89952a0b28428cba26a2eeb464c871fdd7b7a8c0
yarenska <silagunes97@gmail.com>
2025-07-11 21:31:07 +0300

    event message entity

---
+++ src/main/java/com/example/notification_service/entity/EventMessage.java
@@ -0,0 +1,15 @@
+package com.example.notification_service.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class EventMessage {
+    private String id;
+    private String content;
+}
